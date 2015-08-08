e1e7f6f29b11eee06d59142a7f8c6ec3532ce6b4
Christophe Turle <cturle@yahoo.com>
2015-08-08 17:21:55 +0200

    change some colors

--- src/twotris/view.cljs
+++ src/twotris/view.cljs
@@ -17,7 +17,7 @@
                :running   "Playing ..."
                :game-over "Game Over"} )
 
-(def +colors+ ["#181818", "#585858", "#D8D8D8", "#AB4642", "#DC9656", "#F7CA88", "#A1B56C", "#86C1B9", "#7CAFC2", "#BA8BAF", "#A16946"])
+(def +colors+ ["#ff0000", "#00ff00", "#0000ff", "#AB4642", "#DC9656", "#F7CA88", "#A1B56C", "#86C1B9", "#7CAFC2", "#BA8BAF", "#A16946"])
 
 (def action=>description
   {game/rotate          "rotate"
